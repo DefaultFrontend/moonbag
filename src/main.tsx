3cfbf1c9d985924f2566a17b60c24b9f32f72606
DefaultFrontend <default-crypto-cat@proton.me>
2024-11-27 18:43:49 +0200

    routes fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={'/moonbag/'}>
+      <Routes>
+        <Route path='/' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
